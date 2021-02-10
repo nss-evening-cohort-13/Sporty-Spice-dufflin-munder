ae8a58272131847c81c9ea21c000d256393c0dae
Sam Mudick <smudick@gmail.com>
2021-02-09 18:15:42 -0600

    test3

--- DufflinMunder/Program.cs
+++ DufflinMunder/Program.cs
@@ -6,7 +6,7 @@ namespace DufflinMunder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing pushes");
         }
     }
 }
